053fe316631b40b6ba5be83aa3ce6bfc2947769b
Francisco Sabino <fdasabino@gmail.com>
2022-09-21 17:46:52 +0200

    added paralax effect and mosaic section

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 // <!-- Initialize Swiper -->
-// var swiper = new Swiper(".mySwiper", {
-//   effect: "coverflow",
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: 3,
-//   coverflowEffect: {
-//     rotate: 2,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 10,
-//     slideShadows: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-//   loop: true,
-// });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  allowSlidePrev: true,
+  parallax: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
 // mobile navigation toggler
 const toggleMenu = () => {
   const menuToggle = document.querySelector(".toggle");
   menuToggle.classList.toggle("show");
+
   const navigation = document.querySelector(".navigation");
   navigation.classList.toggle("show");
+
+  const bodyOverflow = document.body.style.overflow;
+  document.body.style.overflow = bodyOverflow === "hidden" ? "scroll" : "hidden";
 };
 const closeMenuOnClick = () => {
   toggleMenu(false);
-  console.log("click links");
 };
 
 // active link
 const linkContainer = document.querySelector(".navigation");
 const links = linkContainer.getElementsByTagName("a");
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
+for (const link of links) {
+  link.addEventListener("click", function () {
+    const current = document.getElementsByClassName("active");
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active", "");
     }
